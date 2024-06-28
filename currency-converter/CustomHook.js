a63e908bfaf4a24278dbc9e7f23e1c3f4f37c4d0
Muhammad Ali Ramzan <143883433+ely-ramzan@users.noreply.github.com>
2024-06-28 23:11:25 +0500

    Add files via upload

---
+++ currency-converter/CustomHook.js
@@ -0,0 +1,23 @@
+import {useState,useEffect} from 'react'
+
+const useCurrencyInfo  = function (currency) {
+    const [data, setData] = useState({});
+
+    useEffect(
+            () => {
+            
+            let url = `https://cdn.jsdelivr.net/npm/@fawazahmed0/currency-api@latest/v1/currencies/${currency}.json`;
+
+            fetch(url).
+            then(res => res.json()).
+            then(res => setData(res[currency]))
+
+        },
+            [currency]
+    )
+
+    console.log(data);
+    return data;
+}
+
+export default useCurrencyInfo
